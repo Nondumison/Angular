3d362a53325f3793ecd3f644e4d6abbbb20fc406
Nondumison <37531298+Nondumison@users.noreply.github.com>
2020-03-31 21:40:30 +0200

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const { Entries, Types } = require("../sequelize");
+const router = express.Router();
+
+// Home router
+router.get("/", (req, res) => {
+  console.log(res.json("Hello world"));
+});
+
+// Create Expense Type
+router.post("/Api/type", (req, res) => {
+  console.log(req.body);
+  Types.create(req.body).then(types => res.json(types));
+});
+// Create Expense Entries
+router.post("/Api/entry", (req, res) => {
+  console.log("expense types ==>", req.body);
+  Entries.create(req.body).then(entries => res.json(entries));
+});
+
+// Get all Expense Types
+router.get("/Api/types", (req, res) => {
+  console.log("ROUTE works");
+  Types.findAll().then(types => res.json(types));
+});
+// get all Expense Entries
+router.get("/Api/entries", (req, res) => {
+  Entries.findAll().then(entries => res.json(entries));
+});
+
+// Delete Expense Types form
+router.get("/:id/delete", (req, res, next) => {
+  Types.findByPk(req.params.id).then(types => {
+    res.render("articles/delete", {
+      types: types,
+      title: "Delete Expense Types"
+    });
+  });
+});
+
+// DELETE individual Type.
+router.delete("/:id", (req, res, next) => {
+  Types.findByPk(req.params.id)
+    .then(types => {
+      return types.destroy();
+    })
+    .then(() => {
+      res.redirect("/Api/types");
+    });
+});
+
+module.exports = router;
